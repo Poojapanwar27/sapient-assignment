ca606e6b61a5dc11e449a4f98dfae673dda0d3ed
Pooja Singh <psingh24@sapient.com>
2019-03-06 19:56:11 +0530

    CHANGE SITE ID

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ set -e
 
 # https://gist.github.com/cjus/1047794
 echo 'Retrieving latest deploy...'
-url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/hacker-clone/deploys`
+url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/15d5153e-5a39-4448-bf30-96306aa477e8/deploys`
 temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
 
 # https://www.netlify.com/docs/api/#deploys
 echo "Publishing build ${temp##*|}..."
-curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/hacker-clone/deploys/${temp##*|}/restore"
+curl -X POST -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" -d "{}" "https://api.netlify.com/api/v1/sites/15d5153e-5a39-4448-bf30-96306aa477e8/deploys/${temp##*|}/restore"
